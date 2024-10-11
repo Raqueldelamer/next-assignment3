c0aaf667aa143dec22bee6380a0838f2b2e5cf74
Raquel Marie <raqueldelamer@gmail.com>
2024-10-10 20:43:57 -0500

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { say } from 'cowsay';
+
 export default function HomePage() {
-  return  <div>Hola! 
+  return  <div>
+
+  <h1>Hola!</h1> 
 
   </div>
 } 
