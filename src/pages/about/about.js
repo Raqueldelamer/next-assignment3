541dc99f8de44a8973041618cbad45ed6aa181d4
Raquel Marie <raqueldelamer@gmail.com>
2024-10-13 23:57:22 -0500

    update about.js

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,1 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function AboutPage() {
+    return  (
+    <div>
+
+    <h1>About</h1> 
+    <p>My name is Raquel. I'm feeling very sick at the moment but will add more
+        when I am feeling better.
+    </p>
+
+    </div>
+    );
+} 
--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,1 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function GalleryPage() {
+    return  <div>
+
+    <h1>Gallery</h1> 
+
+    </div>
+} 
