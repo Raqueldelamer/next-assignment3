1c19c95f329f18dcae3b02c515b79319b6376843
Raquel Marie <raqueldelamer@gmail.com>
2024-10-14 16:56:22 -0500

    update globals.css , imgs folder , & about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
     return  (
     <div>
 
-    <h1>About</h1> 
+    <h1>ABOUT</h1> 
     <p>All the images are captured by Raquel Marie with a Canon Power Shot SX, an analog Minolta Xd 11, an Olympus OM-D, or an iPhone.</p>
     
     <p>Inspired and influenced by street photography during the late 19th century to the early and mid 20th century, Raquel Marie’s personal style of photography is predominately black and white, as I feel that it makes my photographs look timeless with more of a focus on shadows, natural light, and composition. 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function HomePage() {
   const divStyle = {
-    backgroundImage: `url(imgs/pexels-cemetery.jpg)`,
+    backgroundImage: `url(imgs/pexels-cemetery1.jpg)`,
     backgroundSize: 'cover', 
     backgroundPosition: 'center',
     height: '100vh', 
